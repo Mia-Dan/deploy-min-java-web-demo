8932b0ce6200ccb0a0021f44fff85976ff70e95e
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2024-02-15 20:17:25 +0800

    init - runnable using IDEA

---
+++ src/main/java/com/example/MinWebDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinWebDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinWebDemoApplication.class, args);
+    }
+
+}
